35b98027dcbb6ca5a06f89df67d309e67921ecc7
anand-887723-stack <anand887723@gmail.com>
2024-11-02 19:15:44 +0530

    just added home functiion and wa

--- app.py
+++ app.py
@@ -14,38 +14,38 @@ def home():
 def whatsapp_reply():
     incoming_msg = request.values.get('Body', '').lower()
     print(incoming_msg)
-    return "okay nice  "
+   
 
 
-#     response = MessagingResponse()
-#     message = response.message()
-#     return "hello "
-#     # print(incoming_msg)
+    response = MessagingResponse()
+    message = response.message()
+    return "hello "
+    # print(incoming_msg)
 
 
-#     parts = incoming_msg.split()
-#     if len(parts) == 2:
-#         day = parts[0]
-#         meal = parts[1]
+    parts = incoming_msg.split()
+    if len(parts) == 2:
+        day = parts[0]
+        meal = parts[1]
         
 
-#         menu_items = data_loader.get_menu(day, meal)
+        menu_items = data_loader.get_menu(day, meal)
         
-#         # Format the response message
-#         if isinstance(menu_items, list):
-#             # Join the list of items into a string
-#             menu_response = f"{day.capitalize()} {meal.capitalize()} menu:\n" + "\n".join(menu_items)
-#         else:
-#             # Handle cases where the response is not a list (error message)
-#             menu_response = menu_items
-#     else:
-#         # Default response for unrecognized format
-#         menu_response = 'Please provide a valid input in the format "day meal" (e.g., "monday dinner").'
+        # Format the response message
+        if isinstance(menu_items, list):
+            # Join the list of items into a string
+            menu_response = f"{day.capitalize()} {meal.capitalize()} menu:\n" + "\n".join(menu_items)
+        else:
+            # Handle cases where the response is not a list (error message)
+            menu_response = menu_items
+    else:
+        # Default response for unrecognized format
+        menu_response = 'Please provide a valid input in the format "day meal" (e.g., "monday dinner").'
 
-#     # Set the response message
-#     message.body(menu_response)
+    # Set the response message
+    message.body(menu_response)
 
-#     return str(response)
+    return str(response)
 
 
 
